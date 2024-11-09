855682ec177cf2bddddf6986d90afa98a3c897bd
unknown <Ashik-Rahman100>
2024-11-09 16:00:10 +0600

    refactoring

--- src/components/task/TaskCard.jsx
+++ src/components/task/TaskCard.jsx
@@ -1,19 +1,11 @@
 import React from "react";
 
 import { CustomScroll } from "react-custom-scroll";
+import { taskData } from "../../utils/task";
 import SingleTaskContent from "./singleTaskContent";
 
 export default function TaskCard({task}) {
-  const taskData = [
-    {id:1, clientName:'Adani'},
-    {id:2, clientName:'Josef'},
-    {id:3, clientName:'Miler'},
-    {id:4, clientName:'Jhon'},
-    {id:5, clientName:'Faizan'},
-    {id:6, clientName:'William'},
-    {id:7, clientName:'Smith'},
-    {id:7, clientName:'Adani'},
-  ]
+  
   return (
     <div className="bg-slate-100 ">
       <div className="flex justify-between p-3 ">
---
+++ src/utils/task.js
@@ -0,0 +1,20 @@
+export const taskData = [
+    {id:1, clientName:'Adani'},
+    {id:2, clientName:'Josef'},
+    {id:3, clientName:'Miler'},
+    {id:4, clientName:'Jhon'},
+    {id:5, clientName:'Faizan'},
+    {id:6, clientName:'William'},
+    {id:7, clientName:'Smith'},
+    {id:7, clientName:'Adani'},
+  ];
+
+export const taskStatus = [
+    {id:1,title:"Incomplete", done:0},
+    {id:2,title:"Painding", done:12},
+    {id:3,title:"Review", done:5},
+    {id:4,title:"Review", done:6},
+    {id:5,title:"Review", done:4},
+    {id:6,title:"Complete", done:1},
+    {id:7,title:"Complete", done:2},
+  ]
